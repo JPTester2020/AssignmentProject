23aa6c34d14218a591294cd607e19aa4d05cd211
SvetlanaLutz <naswlutz@gmail.com>
2021-01-11 20:08:28 +0100

    Heslth random levels fire
    Not finish some problems with orking code
    but code and sprites finish one

--- The lost soul/Assets/Scripts/Arrow.cs
+++ The lost soul/Assets/Scripts/Arrow.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
+    private bool isFirstColor = true;
     public static bool isColor = true;
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -12,20 +13,23 @@ public class Arrow : MonoBehaviour
         {
             Bow.isArrow = true;
             Bow.isMouseDown = false;
+            GameController.isFirstScore = true;
             Destroy(gameObject, 0.3f);
         }
 
         if (collision.gameObject.CompareTag("Color"))
         {
-            ColorChange(collision);
-
-            if (collision.gameObject.name == Character.nameColor) isColor = false;
-            else isColor = true;
+            if(isFirstColor) ColorChange(collision);
         }
     }
 
     private void ColorChange(Collider2D collision)
     {
+        isFirstColor = false;
+
+        if (collision.gameObject.name == Enemy.nameColor) isColor = false;
+        else isColor = true;
+
         SpriteRenderer _spriteRenderer = GetComponent<SpriteRenderer>();
         _spriteRenderer.color = collision.GetComponent<SpriteRenderer>().color;
     }
@@ -36,9 +40,9 @@ public class Arrow : MonoBehaviour
         {
             //GetComponent<PolygonCollider2D>().enabled = false;
 
-            //Bow.isMouseDown = false;
-
-            //Destroy(gameObject);
+            Bow.isMouseDown = false;
+            Bow.isArrow = true;
+            Destroy(gameObject);
         }
     }
 }
